f7045527bfb9ec59f1381028507e8719bb479e74
Mayank Kumar <53262974+MayankKumar10@users.noreply.github.com>
2022-02-19 23:16:03 +0530

    Header, Footer and Home Page
    Adding Header, Footer and Home Page into Mini Project

---
+++ src/scripts/quiz.js
@@ -0,0 +1,13 @@
+var a = document.querySelector(".quiz-cl");
+
+let b = document.querySelector(".btnText");
+
+let c = document.querySelector('.bt2');
+
+b.addEventListener('click', ab());
+
+
+let ab = () =>{
+return a.innerHTML ="abc";
+}
+
